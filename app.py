c2668f02e61253e46ab94b241e770760539f87fc
Arthur Chiquet <arthur.chiquet@gmail.com>
2021-01-20 17:19:29 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,130 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+from server import app
+import pandas as pd
+import json
+import pickle
+import plotly.express as px
+
+mapbox_token = 'pk.eyJ1IjoiYXJ0aHVyY2hpcXVldCIsImEiOiJja2E1bDc3cjYwMTh5M2V0ZzdvbmF5NXB5In0.ETylJ3ztuDA-S3tQmNGpPQ'
+
+
+colors = {
+    'background': '#222222',
+    'text': 'white'
+}
+
+def empty_figure():
+    fig = {
+        'data': [],
+        'layout': {
+        'plot_bgcolor': colors['background'],
+        'paper_bgcolor': colors['background'],
+        'font': {
+            'color': colors['text']}
+        }
+    }
+    return fig
+
+def create_figure(df, geojson):
+    fig = px.choropleth_mapbox(
+        df,
+        geojson=geojson,
+        locations='id',
+        zoom=13,
+        center = {"lat": 49.0686, "lon":3.9695},
+        color='commune',
+        opacity=1,
+        hover_name='numero',
+        color_discrete_map={
+            '51210':'#7FFFD4',
+            '51287':'#FF7F50',
+            '51119':'#6495ED',
+            '51030':'#FF1493'
+        },
+        hover_data={
+            'contenance':True,
+            'commune':True
+        }
+    )
+    fig.update_layout(
+        showlegend=False,
+        height=550,
+        clickmode='event+select',
+        mapbox_style="satellite",
+        mapbox_accesstoken=mapbox_token,
+        margin=dict(l=0, r=0, t=0, b=0))
+
+    fig.update_traces(marker=dict(line=dict(color='black')))
+    return fig
+
+app.layout=html.Div(
+    [
+        html.Br(),
+        html.Div(id="page-content"),
+        dbc.Row(html.H3('Arthur Systems'), justify='center'),
+        html.Br(),
+        dbc.Container(
+            [
+                dcc.Graph(
+                    id='map',
+                    figure=empty_figure()
+                    ),
+                html.Br(),
+                dbc.Row(html.H3(id='surface'), justify='center'),
+                # html.Pre(id='test'),
+            ]
+        ),
+    ]
+)
+
+
+@app.callback(
+    Output('map', 'figure'),
+    Input('page-content', 'children'))
+def display_map(page_content):
+    with open('data/data.json') as file:
+        geojson=json.load(file)
+
+    with open("data/parcelles_J.txt", "rb") as fp:
+        filter_list = pickle.load(fp)
+
+    df=pd.read_csv('data/df.csv')
+    df=df[df.id.isin(filter_list)]
+    df.commune=df.commune.astype('str')
+    features = geojson['features']
+    filtered_features = []
+
+    for feature in features:
+        if feature['id'] in filter_list:
+            filtered_features.append(feature)
+        else:
+            pass
+
+    geojson_v2={'type':'FeatureCollection','features':filtered_features}
+
+    return create_figure(df, geojson_v2)
+
+@app.callback(
+    Output('surface', 'children'),
+    Input('map','selectedData'))
+def return_surface(selectedData):
+    try:
+        surface=0
+        for i in selectedData['points']:
+            surface+=i['customdata'][0]
+        return f'{round(surface/10000, 3)} ha'
+    except:
+        return ''
+
+
+# @app.callback(
+#     Output('test', 'children'),
+#     Input('map','selectedData'))
+# def save_list(selectedData):
+#     return json.dumps(selectedData, indent=2)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ server.py
@@ -0,0 +1,12 @@
+# Dash app initialization
+import dash
+import dash_bootstrap_components as dbc
+
+
+app = dash.Dash(__name__,
+    external_stylesheets=[dbc.themes.DARKLY],
+)
+
+server = app.server
+app.config.suppress_callback_exceptions = True
+
